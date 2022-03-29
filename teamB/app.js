07fb62151718f79652a7e502d77d90ad3413a469
Pratik Majumdar (codadept) <info.pratikmajumdar@gmail.com>
2022-03-30 03:07:16 +0530

    ADD: Basic setup add

--- teamB/app.js
+++
@@ -1,6 +0,0 @@
-const navbarMobile = document.querySelector("span.navbar-mobile");
-const navbar = document.querySelector(".navbar");
-
-navbarMobile.addEventListener("click", () => {
-  navbar.classList.toggle("navbar-show");
-});
