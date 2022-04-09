8950e5e795655caff47b2a3710b5edd7e74787db
biley02 <57981298+biley02@users.noreply.github.com>
2022-04-09 12:27:55 +0530

    cleaned

--- controllers/home.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {import("express").Request} req
- * @param {import("express").Response} res
- */
-const getHomePage = (req, res) => {
-  res.render("home");
-};
-
-export { getHomePage };
--- routes/home.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getHomePage } from "../controllers/home.js";
-
-const route = Router();
-
-route.get("/", getHomePage);
-
-export default route;
