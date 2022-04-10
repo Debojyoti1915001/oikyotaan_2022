08f1e59668df0d65f9f0876eae4ef6f243fed8d5
guilefoylegaurav <grvb1515@gmail.com>
2022-04-10 17:07:31 +0530

    Transfer statics to views along with allied styling. #28

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getHomePage } from "../controllers/home.js";
-import { getGallery } from "../controllers/home.js";
+import { getHomePage, getGallery, getEvents } from "../controllers/home.js";
 //import { gallery} from "../views/gallery.ejs";
 
 const route = Router();
 
 route.get("/", getHomePage);
 route.get("/gallery", getGallery);
+route.get("/events", getEvents);
+
 
 export default route;
